f1a0aa2ccfd6e1edc8b5c8d20734d07078990903
Marcosozhol <3956@holbertonschool.com>
2022-06-16 12:08:55 -0300

    login codes

--- lib/pages/reg_page.dart
+++ lib/pages/reg_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './profile_screen.dart';
 
 // ignore: camel_case_types
 class Page_2 extends StatelessWidget {
@@ -14,11 +17,157 @@ class Page_2 extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            Text(
-              'register page'
-            ),
+            Text('register page'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// borrador
+class _HomePageState extends State {
+  //initialize Firebase
+  Future _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder(
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LoginScreen();
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    ));
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State {
+  //login function
+  static Future loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email');
+      }
+    }
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Creating the textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Login to your App",
+            style: TextStyle(
+                color: Colors.black45,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            "My App Title",
+            style: TextStyle(
+                color: Colors.black45,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.black45,
+                )),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.black45,
+                )),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "He olvidado mi contraseña",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0XFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0)),
+              onPressed: () async {
+                //Let's test the app
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                }
+              },
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
