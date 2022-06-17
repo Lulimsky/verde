59d692dbb084c963f4f17bed951fbf4d11d4e98b
Marcosozhol <3956@holbertonschool.com>
2022-06-16 23:48:32 -0300

    Correccion de error scroll

--- lib/pages/reg_page.dart
+++ lib/pages/reg_page.dart
@@ -17,16 +17,16 @@ class _HomePageState extends StatelessWidget {
           title: Text('Register_Page'),
         ),
         body: FutureBuilder(
-      future: _initializeFirebase(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return LoginScreen();
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    ));
+          future: _initializeFirebase(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return LoginScreen();
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
 
@@ -63,92 +63,94 @@ class _LoginScreenState extends State {
     TextEditingController _emailController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
 
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Login to your App",
-            style: TextStyle(
-                color: Colors.black45,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            "My App Title",
-            style: TextStyle(
-                color: Colors.black45,
-                fontSize: 44.0,
-                fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          TextField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-                hintText: "Email",
-                prefixIcon: Icon(
-                  Icons.email,
+    return Scaffold(
+      body: Container(
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Login to your App",
+              style: TextStyle(
                   color: Colors.black45,
-                )),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(
-                hintText: "Password",
-                prefixIcon: Icon(
-                  Icons.lock,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "My App Title",
+              style: TextStyle(
                   color: Colors.black45,
-                )),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text(
-            "He olvidado mi contraseña",
-            style: TextStyle(color: Colors.blue),
-          ),
-          const SizedBox(
-            height: 88.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: RawMaterialButton(
-              fillColor: const Color(0XFF0069FE),
-              elevation: 0.0,
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4.0)),
-              onPressed: () async {
-                //Let's test the app
-                User? user = await loginUsingEmailPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                    context: context);
-                if (user != null) {
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ProfileScreen()));
-                }
-              },
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+                  fontSize: 44.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.black45,
+                  )),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black45,
+                  )),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const Text(
+              "He olvidado mi contraseña",
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 88.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: const Color(0XFF0069FE),
+                elevation: 0.0,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0)),
+                onPressed: () async {
+                  //Let's test the app
+                  User? user = await loginUsingEmailPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      context: context);
+                  if (user != null) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => ProfileScreen()));
+                  }
+                },
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
-      ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
